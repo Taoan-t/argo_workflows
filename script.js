345f11694dbf3cbea2779bd0524a53e7d7f73a70
Qian Tang <qtang12@myseneca.ca>
2024-12-08 21:08:35 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Main function to execute the script
+function main() {
+  const result = addNumbers(5, 10);
+  console.log(`The result of adding 5 and 10 is: ${result}`);
+}
+
+// Run the main function
+main();
